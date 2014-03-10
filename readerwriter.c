f9a31411abb9a72b65a562e988d79a29d07d94c1
Karl Malbrain <malbrain@cal.berkeley.edu>
2014-03-10 14:56:03 -0700

    Initial program release full source

--- readerwriter.c
+++ readerwriter.c
@@ -1,2 +1,41 @@
-// Phase fair reader/writer lock
+//  return 1 if obtained,
+//      0 otherwise
 
+int bt_spinwritetry(BtSpinLatch *latch)
+{
+ushort prev;
+
+#ifdef  unix
+    prev = __sync_fetch_and_or((ushort *)latch, XCL);
+#else
+    _InterlockedOr16((ushort *)latch, XCL);
+#endif
+    //  take write access if all bits are clear
+
+    if( !(prev & XCL) )
+      if( !(prev & ~BOTH) )
+        return 1;
+      else
+#ifdef unix
+        __sync_fetch_and_and ((ushort *)latch, ~XCL);
+#else
+        _InterlockedAnd16((ushort *)latch, ~XCL);
+#endif
+    return 0;
+}
+
+//  clear write mode
+
+void bt_spinreleasewrite(BtSpinLatch *latch)
+{
+#ifdef unix
+    __sync_fetch_and_and((ushort *)latch, ~BOTH);
+#else
+    _InterlockedAnd16((ushort *)latch, ~BOTH);
+#endif
+}
+
+//  decrement reader count
+
+void bt_spinreleaseread(BtSpinLatch *latch)
+{
